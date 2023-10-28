b8bd1870597359a3ec5865e6dc7d5ccf6c40e878
iamapusaha <apusaha392@gmail.com>
2023-10-28 10:51:42 +0600

    added header

--- src/Routers/Routers.jsx
+++ src/Routers/Routers.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
+import Home from "../Pages/Home/Home/Home";
 
 const router = createBrowserRouter([
     {
@@ -7,7 +8,8 @@ const router = createBrowserRouter([
         element: <Main></Main>,
         children: [
             {
-
+                path: '/',
+                element: <Home></Home>
             }
         ]
     },
