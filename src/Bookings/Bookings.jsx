3ebb9ea6fff902b3e865dcf3f54ddb681fac86e6
iamapusaha <apusaha392@gmail.com>
2023-11-01 20:07:36 +0600

    create jwt token

--- src/Bookings/Bookings.jsx
+++ src/Bookings/Bookings.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../Providers/AuthProviders";
 import BookingItems from "./BookingItems";
+import axios from "axios";
 
 
 const Bookings = () => {
@@ -8,11 +9,15 @@ const Bookings = () => {
     const [bookings, setBookings] = useState([])
     const url = `http://localhost:5000/bookings?email=${user?.email}`
     useEffect(() => {
-        fetch(url)
-            .then(res => res.json())
-            .then(data => {
-                setBookings(data);
+        axios.get(url, { withCredentials: true })
+            .then(res => {
+                setBookings(res.data)
             })
+        // fetch(url)
+        //     .then(res => res.json())
+        //     .then(data => {
+        //         setBookings(data);
+        //     })
     }, [url])
 
     const handleDelete = id => {
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -21,10 +21,13 @@ const Login = () => {
                 const loggedInUser = result.user;
                 console.log(loggedInUser);
                 const user = { email }
-                // navigate(location?.state ? location?.state : '/')
-                axios.post('http://localhost:5000/jwt', user)
+                navigate(location?.state ? location?.state : '/')
+                axios.post('http://localhost:5000/jwt', user, { withCredentials: true })
                     .then(res => {
                         console.log(res.data);
+                        if (res.data.success) {
+                            navigate(location?.state ? location?.state : '/')
+                        }
                     })
             })
             .catch(error => {
